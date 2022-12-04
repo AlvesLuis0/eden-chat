8e683368edf3055dc534a4ee04b1bbef3a567f2e
AlvesLuis0 <alves.luis.3002@gmail.com>
2022-12-04 15:42:05 -0300

    login page

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,19 +1,10 @@
 // importing important modules
 import { io } from "./http";
-import { Message } from "./model/Message";
-
-
-// declaring import variables
-const messagesList: Message[] = [];
 
 
 // applying functions to websockets
 io.on("connection", (socket) => {
-	socket.emit("setup", messagesList);
 
-	socket.on("sendMessage", (message: Message) => {
-		message.date = new Date().toTimeString().slice(0,5);
-		messagesList.push(message);
-		socket.broadcast.emit("receiveMessage", message);
-	});
+	
+
 });
