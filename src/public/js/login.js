4acb5d73c964f5d282f900b3d8b500e21deb328a
AlvesLuis0 <alves.luis.3002@gmail.com>
2022-12-05 20:33:58 -0300

    login script created

---
+++ src/public/js/login.js
@@ -0,0 +1,6 @@
+document.forms[0].addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const username = event.target[0].value;
+  location.href = "/chat.html/?username=" + username;
+});
